7189729ef9a3a024f6c56071fbfc705326104956
Joe Sasson <sassonjoe66@gmail.com>
2023-08-04 11:21:19 -0400

    updated again

--- app.py
+++ app.py
@@ -16,32 +16,8 @@ from worker import r
 from rq.job import Job
 from tasks import process_upload
 
-# Get the current date and time
-now = datetime.now()
-
-# Convert the current date and time into a string format: 'Year-Month-Day_Hour-Minute-Second'
-date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-
-# Create a filename for log file
-log_filename = f'app_{date_time}.log'
-
-# Create a logger
-logger = logging.getLogger('app')
-
-# Set logger level
-logger.setLevel(logging.INFO)
-
-# Create a handler that writes to stderr
-handler = logging.StreamHandler(stream=sys.stderr)
-
-# Set a format for the messages
-formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-
-# Set the format for the handler
-handler.setFormatter(formatter)
-
-# Add the handler to the logger
-logger.addHandler(handler)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
@@ -70,10 +46,10 @@ def upload():
         str: Response message and success indication
     """
     try:
-        logger.info('Processing upload request')
+        logging.info('Processing upload request')
         
         if 'file' not in request.files:
-            logger.error('No file part in request')
+            logging.error('No file part in request')
             return jsonify({"success": False, "message": "No file part"})
 
         file = request.files['file']
@@ -81,7 +57,7 @@ def upload():
         fields = request.form['fields']
 
         if file.filename == '':
-            logger.error('No file selected for uploading')
+            logging.error('No file selected for uploading')
             return jsonify({"success": False, "message": "No file selected for uploading"})
 
         if file:
@@ -95,7 +71,7 @@ def upload():
                 "job_id": job.get_id()
             })
     except Exception as e:
-        logger.error('Exception occurred', exc_info=True)
+        logging.error('Exception occurred', exc_info=True)
         return jsonify({"success": False, "message": "An error occurred while processing the request"})
     
 @app.route('/job/status/<job_id>', methods=['GET'])
@@ -104,29 +80,29 @@ def job_status(job_id):
     Check the status of a job
     """
     try:
-        logger.info(f'Fetching job with id: {job_id}')
+        logging.info(f'Fetching job with id: {job_id}')
         job = Job.fetch(job_id, connection=r)
     except NoSuchJobError:
-        logger.error(f'No such job: {job_id}')
+        logging.error(f'No such job: {job_id}')
         return {"success": False, "message": f"No such job: {job_id}"}
     except Exception as e:
-        logger.error('Exception occurred while fetching job status', exc_info=True)
+        logging.error('Exception occurred while fetching job status', exc_info=True)
         return {"success": False, "message": "An error occurred while checking the job status"}
 
     if job.is_finished:
-        logger.info(f'Job {job_id} has finished')
+        logging.info(f'Job {job_id} has finished')
         return {"status": "finished"}
     elif job.is_queued:
-        logger.info(f'Job {job_id} is in the queue')
+        logging.info(f'Job {job_id} is in the queue')
         return {"status": "queued"}
     elif job.is_started:
-        logger.info(f'Job {job_id} has started')
+        logging.info(f'Job {job_id} has started')
         return {"status": "started"}
     elif job.is_failed:
-        logger.info(f'Job {job_id} has failed')
+        logging.info(f'Job {job_id} has failed')
         return {"status": "failed"}
     else:
-        logger.warning(f'Job {job_id} status is unknown')
+        logging.warning(f'Job {job_id} status is unknown')
         return {"status": "unknown"}
 
 @app.route('/job/result/<job_id>', methods=['GET'])
@@ -135,25 +111,25 @@ def job_result(job_id):
     Get the result of a job
     """
     try:
-        logger.info(f'Fetching result for job id: {job_id}')
+        logging.info(f'Fetching result for job id: {job_id}')
         job = Job.fetch(job_id, connection=r)
     except NoSuchJobError:
-        logger.error(f'No such job: {job_id}')
+        logging.error(f'No such job: {job_id}')
         return {"success": False, "message": f"No such job: {job_id}"}
     except Exception as e:
-        logger.error('Exception occurred while fetching job result', exc_info=True)
+        logging.error('Exception occurred while fetching job result', exc_info=True)
         return {"success": False, "message": "An error occurred while fetching the job result"}
 
     if job.is_finished:
         if job.result.get("success"):
-            logger.info(f'Successfully retrieved result for job id: {job_id}')
-            logger.info(f'Data: {job.result.get("ocr_result")}')
+            logging.info(f'Successfully retrieved result for job id: {job_id}')
+            logging.info(f'Data: {job.result.get("ocr_result")}')
             return {"success":True, "ocr_result": job.result.get("ocr_result")}
         else:
-            logger.error(f'Error during processing for job id: {job_id}, Message: {job.result.get("message")}')
+            logging.error(f'Error during processing for job id: {job_id}, Message: {job.result.get("message")}')
             return {"success": False, "message": "Error during processing", "error": job.result.get("message")}
     else:
-        logger.info(f'Job {job_id} is not finished yet')
+        logging.info(f'Job {job_id} is not finished yet')
         return {"success": False, "message": "Job not finished"}
 
 
