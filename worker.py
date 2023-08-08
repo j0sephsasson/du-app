9b8cc21c8d2a8d888535574945d910e0cc7935bc
Joe Sasson <sassonjoe66@gmail.com>
2023-08-07 23:12:06 -0400

    attached prod redis worker

--- worker.py
+++ worker.py
@@ -1,11 +1,16 @@
 import os
 from rq import Queue, Worker, Connection
 import redis
+from urllib.parse import urlparse
 
 listen = ['high', 'default', 'low']
 
-# Connect to Redis
-r = redis.Redis(host='localhost', port=6379)
+# Connect to Redis locally
+# r = redis.Redis(host='localhost', port=6379)
+
+# Connect to Redis production
+url = urlparse(os.getenv("REDIS_URL"))
+r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None) # connect to redis - via Heroku docs
 
 # Set up the RQ queue
 queue = Queue(connection=r)
