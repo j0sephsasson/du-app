a87375e5fa8386cf72c0faea136a1ffd1ceda525
Joe Sasson <sassonjoe66@gmail.com>
2023-08-14 15:11:56 -0400

    fixed timeout forreal!

--- worker.py
+++ worker.py
@@ -16,7 +16,7 @@ url = urlparse(os.getenv("REDIS_URL"))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None) # connect to redis - via Heroku docs
 
 # Set up the RQ queue
-queue = Queue(connection=r)
+queue = Queue(connection=r, default_timeout=300)
 
 if __name__ == '__main__':
     with Connection(r):
